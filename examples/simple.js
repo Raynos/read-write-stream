1b26fd07cf9d47ebc6cd767e506eb557860fe691
Raynos <raynos2@gmail.com>
2012-09-09 04:52:32 -0700

    updated example

--- examples/simple.js
+++ examples/simple.js
@@ -3,11 +3,7 @@ var through = require("..")
     , output = through(function write(chunk) {
         console.log("chunk", chunk)
     })
-    , transform = through(function write(chunk, buffer) {
-        buffer.push(chunk * 2)
-    }, function read(bytes, buffer) {
-        return buffer.shift() * 2
-    })
+    , transform = through(write, read)
 
 input.pipe(transform)
 transform.pipe(output)
@@ -15,3 +11,21 @@ transform.pipe(output)
 input.write(1)
 input.write(2)
 input.write(3)
+
+var to = require("write-stream")
+    , from = require("read-stream")
+    , intern = through(write)
+    , list = []
+
+intern.pipe(to(list, function () {
+    console.log("list", list)
+}))
+from([1,2,3,4]).pipe(intern)
+
+function write(chunk, buffer) {
+    buffer.push(chunk * 2)
+}
+
+function read(bytes, buffer) {
+    return buffer.shift() * 2
+}
